5e6b44363eefdf821fae3b546c8e90254b82d0e1
sk007 <hexiaoyou007@gmail.com>
2020-03-18 10:04:32 +0800

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -23,7 +23,7 @@ if [ $? -eq 0 ]; then
     echo "nameserver 127.0.0.1" > /etc/resolv.conf
     touch /etc/dnsmasq.d/unlock.conf
     echo "server=8.8.8.8" > /etc/dnsmasq.d/unlock.conf
-    echo "server=8.8.4.4" >> /etc/dnsmasq.d/unlock.conf
+    echo "server=1.1.1.1" >> /etc/dnsmasq.d/unlock.conf
     echo "server=/netflix.com/$1" >> /etc/dnsmasq.d/unlock.conf
     echo "server=/netflix.net/$1" >> /etc/dnsmasq.d/unlock.conf
     echo "server=/nflximg.net/$1" >> /etc/dnsmasq.d/unlock.conf
